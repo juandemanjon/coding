13365fa6a2fd74ac780d4e4ef26c2deee4a73fa8
jmanjon <juande.manjon@gmail.com>
2022-02-05 08:51:51 +0000

    Final Solution

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -7,24 +7,157 @@
 
 struct Token
 {
+    Token(char o) : Operator(o) {}
+    Token(int n) : Number(n) {}
+
     char Operator = 0;
 
     int Number = 0;
+
+    bool operator!=(const Token &that) const { return !operator==(that); }
+
+    bool operator==(const Token &that) const
+    {
+        return Operator == that.Operator && Number == that.Number;
+    }
 };
 
-using RPNVector = std::vector<Token>;
-
-
-RPNVector RPNTokenize(std::string_view input)
+std::ostream &operator<<(std::ostream &output, const Token &token)
 {
-    RPNVector output;
-    
+    if (token.Operator)
+        output << '\'' << token.Operator << '\'';
+    else
+        output << token.Number;
+    return output;
+}
+
+using TokenVector = std::vector<Token>;
+
+std::ostream &operator<<(std::ostream &output, const TokenVector &tokens)
+{
+    output << '{';
+    for (const Token &token : tokens)
+    {
+        output << token << ',';
+    }
+    output << '}';
+    return output;
+}
+
+TokenVector Parse(std::string_view input)
+{
+    TokenVector output;
+
+    std::string number;
+
+    for (char c : input)
+    {
+        switch (c)
+        {
+            case ' ':
+                if (!number.empty())
+                {
+                    output.push_back(atoi(number.c_str()));
+                    number.clear();
+                }
+                break;
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '(':
+            case ')':
+                if (!number.empty())
+                {
+                    output.push_back(atoi(number.c_str()));
+                    number.clear();
+                }
+                output.push_back(c);
+                break;
+            default:
+                number += c;
+        }
+    }
+    if (!number.empty())
+    {
+        output.push_back(atoi(number.c_str()));
+    }
+
+    return output;
+}
+
+int priority(const Token &token)
+{
+    switch (token.Operator)
+    {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        default:  // Avoid warnings
+            return 0;
+    }
+    return 0;
+}
+
+TokenVector RPN(const TokenVector &input)
+{
+    TokenVector output;
+
+    std::stack<Token> operators;
+
+    for (const auto &token : input)
+    {
+        if (!token.Operator)
+        {
+            output.push_back(token);
+        }
+        else
+        {
+            if (operators.empty() || token.Operator == '(')
+            {
+                operators.push(token);
+            }
+            else if (token.Operator == ')')
+            {
+                while (!operators.empty() && operators.top().Operator != '(')
+                {
+                    output.push_back(operators.top());
+                    operators.pop();
+                }
+                if (!operators.empty())
+                {
+                    operators.pop();  // remove '('
+                }
+            }
+            else
+            {
+                auto token_priority = priority(token);
+                while (!operators.empty() && priority(operators.top()) > token_priority)
+                {
+                    output.push_back(operators.top());
+                    operators.pop();
+                }
+                operators.push(token);
+            }
+        }
+    }
+
+    while (!operators.empty())
+    {
+        output.push_back(operators.top());
+        operators.pop();
+    }
+
     return output;
 }
 
 double Compute(double value1, double value2, char ope)
 {
-    switch (ope) {
+    switch (ope)
+    {
         case '+':
             return value1 + value2;
         case '-':
@@ -32,31 +165,33 @@ double Compute(double value1, double value2, char ope)
         case '*':
             return value1 * value2;
         case '/':
-            return value1 / value2;            
-        default: // Avoid warnings
+            return value1 / value2;
+        default:  // Avoid warnings
             return 0;
     }
-    
+
     // Non reachable code
     return 0;
 }
 
-
-double Compute(const RPNVector &input)
+double Compute(const TokenVector &input)
 {
     std::stack<double> values;
-        
-    for (const Token &token: input)
+
+    for (const Token &token : input)
     {
-        if (token.Operator) {
+        if (token.Operator)
+        {
+            // Get values. TODO: validate stack is not empty before calling top()
             auto value2 = values.top();
             values.pop();
             auto value1 = values.top();
             values.pop();
-            
+
             values.push(Compute(value1, value2, token.Operator));
-            
-        } else {
+        }
+        else
+        {
             values.push(token.Number);
         }
     }
@@ -65,33 +200,126 @@ double Compute(const RPNVector &input)
 
 TEST_CASE("Parse expressions")
 {
+    struct TestCase
+    {
+        std::string_view exp;
+
+        TokenVector expected;
+    };
+
+    std::vector<TestCase> test_cases(
+        {
+            {"1 + 2",   {1,'+',2,}},
+            {"1 - 2",   {1,'-',2,}},
+            {"1 * 2",   {1,'*',2,}},
+            {"1 / 2",   {1,'/',2,}},
+
+            {"1 + 2 + 3",   {1,'+',2,'+',3,}},
+            {"1 + 2 - 3",   {1,'+',2,'-',3,}},
+            {"1 + 2 * 3",   {1,'+',2,'*',3,}},
+            {"1 + 2 / 3",   {1,'+',2,'/',3,}},
+            {"(1 + 2) * 3",   {'(',1,'+',2,')','*',3,}},
+            {"(1 + 2) / 3",   {'(',1,'+',2,')','/',3,}},
+        }
+    );
     
+    for (const auto &tc: test_cases) {
+        auto got = Parse(tc.exp);
+
+        REQUIRE(got.size() == tc.expected.size());
+        for (size_t i = 0; i < got.size(); ++i) {
+            CHECK(got[i] == tc.expected[i]);
+        }
+    }
 }
 
-struct RPN_Result
+TEST_CASE("RPN tokens")
 {
-    RPNVector rpn;
-    
-    double expected;
-};
+    struct TestCase
+    {
+        TokenVector tokens;
+
+        TokenVector expected;
+    };
+    std::vector<TestCase> test_cases(
+        {
+            { {1,'+',2,},                {1,2,'+',}       },
+            { {1,'+',2,'*',3,},          {1,2,3,'*','+',} },
+            { {'(',1,'+',2,')','*',3,},  {1,2,'+',3,'*',} },
+        }
+    );
+        
+    for (const auto &tc : test_cases) {
+        auto got = RPN(tc.tokens);
+        INFO("got " << got << '\n');
+        REQUIRE(got.size() == tc.expected.size());
+        for (size_t i = 0; i < got.size(); ++i) {
+            CHECK(got[i] == tc.expected[i]);
+        }
+    }
+}
 
 TEST_CASE("Compute RPNs")
 {
-    std::vector<RPN_Result> test_cases(
+    struct TestCase
+    {
+        TokenVector rpn;
+
+        double expected;
+    };
+
+    std::vector<TestCase> test_cases(
         {
-            { {{0, 1},{0, 2},{'+',0},},  3.0, },
-            { {{0, 1},{0, 2},{'-',0},}, -1.0, },
-            { {{0, 1},{0, 2},{'*',0},},  2.0, },
-            { {{0, 1},{0, 2},{'/',0},},  0.5, },
+            { {1,2,'+',},  3.0, },
+            { {1,2,'-',}, -1.0, },
+            { {1,2,'*',},  2.0, },
+            { {1,2,'/',},  0.5, },
             
-            { {{0, 1},{0, 2},{'+',0},{0, 3},{'+',0},},  6.0, },
-            { {{0, 1},{0, 2},{'+',0},{0, 3},{'-',0},},  0.0, },
-            { {{0, 1},{0, 2},{'+',0},{0, 3},{'*',0},},  9.0, },
-            { {{0, 1},{0, 2},{'+',0},{0, 3},{'/',0},},  1.0, },
+            { {1,2,'+',3,'+',},  6.0, },
+            { {1,2,'+',3,'-',},  0.0, },
+            { {1,2,'+',3,'*',},  9.0, },
+            { {1,2,'+',3,'/',},  1.0, },
+            
+            { {1,2,3,'*','+',},  7.0, },
         }
-    );       
+    );        
 
     for (const auto &tc : test_cases) {
+        INFO(tc.rpn);
         REQUIRE(Compute(tc.rpn) == tc.expected);
+    }
+}
+
+TEST_CASE("end to end")
+{
+    struct TestCase
+    {
+        std::string_view exp;
+
+        double expected;
+    };
+
+    std::vector<TestCase> test_cases(
+        {
+            {"1 + 2",   3},
+            {"1 - 2",   -1},
+            {"1 * 2",   2},
+            {"1 / 2",   0.5},
+
+            {"1 + 2 + 3",   6},
+            {"1 + 2 - 3",   0},
+            {"1 + 2 * 3",   7},
+            {"1 + 2 / 4",   1.5},
+            {"1 + (2 * 3)",   7},
+            {"1 + (2 / 4)",   1.5},
+            {"(1 + 2) * 3",   9},
+            {"(1 + 2) / 3",   1},
+        }
+    );
+    
+    for (const auto &tc: test_cases) {
+        auto got = Compute(RPN(Parse(tc.exp)));
+        INFO(tc.exp);
+        REQUIRE(got == tc.expected);
     }
 }
